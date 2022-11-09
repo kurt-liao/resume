48d472b8361229a61ced6fd958f7d039b3d480b2
廖晨竹 <vermouth0226@gmail.com>
2022-11-09 16:57:43 +0800

    spin

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import Alpine from 'alpinejs'
+import { translation } from './constants/translation'
 import header from './components/header'
 import spin from './components/spin'
-import locale from './utils/locale'
 import transferComponent from './utils/transferComponent'
 
 window.Alpine = Alpine
 
-Alpine.data('locale', locale)
+Alpine.store('locale', {
+  on: false,
+  lang: 'en',
+  translations: translation(),
+  t(str: string) {
+    if (!this.translations[str])
+      throw new Error(`${str} Translation string not found`)
+
+    if (!this.translations[str][this.lang])
+      throw new Error(`${str} Translation locale not found`)
+
+    return this.translations[str][this.lang]
+  },
+  setLang(value: string) {
+    this.lang = value
+  },
+})
+
 Alpine.data('layout', () => ({
   header: transferComponent(header()),
   spin: transferComponent(spin()),
