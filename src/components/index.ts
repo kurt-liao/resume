687dbc3af11b032276503d8fb8c15db70d06b8b3
kurt-liao <vermouth0226@gmail.com>
2022-11-14 22:43:48 +0800

    change font, remove no used code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
 import type { Alpine } from 'alpinejs'
 import transferComponent from '../utils/transferComponent'
-import header from './header'
 import spin from './spin'
 
 export default (alpine: Alpine) => {
   alpine.data('components', () => ({
-    header: transferComponent(header()),
     spin: transferComponent(spin()),
   }))
 }
